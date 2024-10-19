e3643305995871a4169295c51eab991fa290b957
datagnss <dev@datagnss.com>
2024-10-19 14:20:54 +0800

    Update AP_GPS_NMEA.h
    remove blank lines

--- libraries/AP_GPS/AP_GPS_NMEA.h
+++ libraries/AP_GPS/AP_GPS_NMEA.h
@@ -272,8 +272,6 @@ private:
     
 
 #if AP_GPS_NMEA_ALLYSTAR_ENABLED
-
-    
 #if GPS_MOVING_BASELINE
     // $PALYSBLS,034727.000,0.256,0.992,-0.258,1.056,75.52,-14.12,R*4E
     // lat-projection of bl,long-project of bl,height-project of bl,baseline length,yaw,pitch,mode(A:Autonomous, D: DGPS, N: invalid,F:Float,R:RTK Fixed)
